c8fd5de1a1dbefea4a3968050353e0e6cd46ad56
Chad Campbell <ccampb13@gmail.com>
2014-06-12 13:58:37 -0500

    working on validate

--- app/lib/sockets.js
+++ app/lib/sockets.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var Cookies = require('cookies');
+var traceur = require('traceur');
+var User;
 
 exports.connection = function(socket){
-  addUserToSocket(socket);
+  if(global.nss){
+    User = traceur.require(__dirname + '/../models/user.js');
+    addUserToSocket(socket);
+  }
 };
 
 /* -------------------------------------------------------------------------- */
@@ -18,6 +23,12 @@ function addUserToSocket(socket){
   if(encoded){
     decoded = decode(encoded);
   }
+
+  User.findById(decoded.userId, user=>{
+    socket.set('user', user, ()=>{
+      socket.emit('online');
+    });
+  });
 
   // 1. Find user in DB
   // 2. Add user to socket
